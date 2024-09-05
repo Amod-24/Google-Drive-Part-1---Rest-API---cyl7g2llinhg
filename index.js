39ee1f6246d986fee30dbead6edf45a1b7dbfb7f
Amod Ranjan <amod@Amods-MacBook-Pro.local>
2024-09-05 15:55:31 +0530

    done

--- index.js
+++ index.js
@@ -18,58 +18,60 @@ app.post("/file/create",(req,res)=>{
 })
 
 app.delete("/file/:filename",(req,res)=>{
-  // const filename = req.params.filename;
-  // if(deleteAFile(filename)){
-  //   res.send({
-  //     message : "File deleted successfully"
-  //   })
-  // }
-  // res.send({
-  //   message : "Provide a valid file name"
-  // })
+  const filename = req.params.filename;
+  if(deleteAFile(filename)){
+    res.send({
+      message : "File deleted successfully"
+    })
+  }
+  res.send({
+    message : "Provide a valid file name"
+  })
 })
 
 app.get("/file",(req,res)=>{
-  // const allFiles = getAllFiles();
-  // res.send({
-  //   files : allFiles
-  // })
+  const allFiles = getAllFiles();
+  res.send({
+    files : allFiles
+  })
 })
 
 app.get("/file/:fileName",(req,res)=>{
-  // const fileName = req.params.fileName;
-  // const ans = getAFile(fileName);
-  // if(ans != false){
-  //   res.send({
-  //     fileContent : ans,
-  //     message : "success"
-  //   })
-  // }
-  // res.send({
-  //   message : "File does not exist"
-  // })
+  const fileName = req.params.fileName;
+  const ans = getAFile(fileName);
+  if(ans != false){
+    res.send({
+      fileContent : ans,
+      message : "success"
+    })
+  }
+  res.send({
+    message : "File does not exist"
+  })
 })
 
 app.put("/file/:filename",(req,res)=>{
-  // const fileName = req.params.filename;
-  // const {updatedFileName, newFileData} = req.body;
-  // if((updatedFileName || newFileData) && fileName){
-  //   const message = updateAfile(fileName,updatedFileName, newFileData);
-  //   if( message == "File updated successfully"){
-  //     res.send({
-  //       message : message
-  //     })
-  //   }
-  //   res.send({
-  //     message : message
-  //   })
-  // }
-  // else{
-  //   res.send({
-  //     message : "Provide all data"
-  //   })
-  // }
+  const fileName = req.params.filename;
+  const {updatedFileName, newFileData} = req.body;
+  if((updatedFileName || newFileData) && fileName){
+    const message = updateAfile(fileName,updatedFileName, newFileData);
+    if( message == "File updated successfully"){
+      res.send({
+        message : message
+      })
+    }
+    res.send({
+      message : message
+    })
+  }
+  else{
+    res.send({
+      message : "Provide all data"
+    })
+  }
 })
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
+
+export default app;
